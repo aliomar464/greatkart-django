9330f123b8d75b1ee320f0d88d4b245c64657902
ali <aliomar464@gmail.coma>
2021-09-11 18:12:42 -0400

    before search bar

--- store/views.py
+++ store/views.py
@@ -19,15 +19,15 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
-        paginator = Paginator(products, 3)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
+        paginator = Paginator(products, 8)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
         'products': paged_products,
-        'product_count': product_count
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
 
